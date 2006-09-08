2fde76e200681c3fd905734d84e1b8578f3f4692
fabiankeil <fabiankeil>
2006-09-08 09:49:23 +0000

    Deliver documents in the user-manual directory with "Content-Type text/css" if their filename ends with ".css".

--- cgisimple.c
+++ cgisimple.c
@@ -1,4 +1,4 @@
-const char cgisimple_rcs[] = "$Id: cgisimple.c,v 1.38 2006/09/06 18:45:03 fabiankeil Exp $";
+const char cgisimple_rcs[] = "$Id: cgisimple.c,v 1.39 2006/09/08 09:49:23 fabiankeil Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/cgisimple.c,v $
@@ -36,6 +36,11 @@ const char cgisimple_rcs[] = "$Id: cgisimple.c,v 1.38 2006/09/06 18:45:03 fabian
  *
  * Revisions   :
  *    $Log: cgisimple.c,v $
+ *    Revision 1.39  2006/09/08 09:49:23  fabiankeil
+ *    Deliver documents in the user-manual directory
+ *    with "Content-Type text/css" if their filename
+ *    ends with ".css".
+ *
  *    Revision 1.38  2006/09/06 18:45:03  fabiankeil
  *    Incorporate modified version of Roland Rosenfeld's patch to
  *    optionally access the user-manual via Privoxy. Closes patch 679075.
@@ -696,6 +701,7 @@ jb_err cgi_send_user_manual(struct client_state *csp,
    FILE *fp;
    char buf[BUFFER_SIZE];
    jb_err err = JB_ERR_OK;
+   size_t length = 0;
 
    assert(csp);
    assert(rsp);
@@ -747,12 +753,21 @@ jb_err cgi_send_user_manual(struct client_state *csp,
    }
    fclose(fp);
 
-   if (enlist(rsp->headers, "Content-Type: text/html"))
+   /* Guess correct Content-Type based on the filename's ending */
+   if (filename)
    {
-      return JB_ERR_MEMORY;
+      length = strlen(filename);
+   }
+   if((length>=4)  && !strcmp(&filename[length-4], ".css"))
+   {
+      err = enlist(rsp->headers, "Content-Type: text/css");
+   }
+   else
+   {
+      err = enlist(rsp->headers, "Content-Type: text/html");
    }
 
-   return JB_ERR_OK;
+   return err;
 }
 
 
