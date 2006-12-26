64f6c51849d006b312fbb73293eeea1327c9265b
fabiankeil <fabiankeil>
2006-12-26 17:38:50 +0000

    Silence compiler warning I introduced with my last commit.

--- jcc.c
+++ jcc.c
@@ -1,4 +1,4 @@
-const char jcc_rcs[] = "$Id: jcc.c,v 1.112 2006/12/26 17:31:41 fabiankeil Exp $";
+const char jcc_rcs[] = "$Id: jcc.c,v 1.113 2006/12/26 17:38:50 fabiankeil Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/jcc.c,v $
@@ -33,6 +33,9 @@ const char jcc_rcs[] = "$Id: jcc.c,v 1.112 2006/12/26 17:31:41 fabiankeil Exp $"
  *
  * Revisions   :
  *    $Log: jcc.c,v $
+ *    Revision 1.113  2006/12/26 17:38:50  fabiankeil
+ *    Silence compiler warning I introduced with my last commit.
+ *
  *    Revision 1.112  2006/12/26 17:31:41  fabiankeil
  *    Mutex protect rand() if POSIX threading
  *    is used, warn the user if that's not possible
@@ -2084,7 +2087,7 @@ int main(int argc, const char *argv[])
 #endif
 {
    int argc_pos = 0;
-   int random_seed;
+   unsigned int random_seed;
 #ifdef unix
    struct passwd *pw = NULL;
    struct group *grp = NULL;
