f2d422e2b4d9cff5a35823047280175a041d2999
fabiankeil <fabiankeil>
2006-12-27 18:52:02 +0000

    Fix -pedantic ISO C warning about converting from function pointer to object pointer.

--- jcc.c
+++ jcc.c
@@ -1,4 +1,4 @@
-const char jcc_rcs[] = "$Id: jcc.c,v 1.113 2006/12/26 17:38:50 fabiankeil Exp $";
+const char jcc_rcs[] = "$Id: jcc.c,v 1.114 2006/12/27 18:52:02 fabiankeil Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/jcc.c,v $
@@ -33,6 +33,10 @@ const char jcc_rcs[] = "$Id: jcc.c,v 1.113 2006/12/26 17:38:50 fabiankeil Exp $"
  *
  * Revisions   :
  *    $Log: jcc.c,v $
+ *    Revision 1.114  2006/12/27 18:52:02  fabiankeil
+ *    Fix -pedantic ISO C warning about converting
+ *    from function pointer to object pointer.
+ *
  *    Revision 1.113  2006/12/26 17:38:50  fabiankeil
  *    Silence compiler warning I introduced with my last commit.
  *
@@ -2722,7 +2726,7 @@ static void listen_loop(void)
             pthread_attr_init(&attrs);
             pthread_attr_setdetachstate(&attrs, PTHREAD_CREATE_DETACHED);
             errno = pthread_create(&the_thread, &attrs,
-               (void*)serve, csp);
+               (void * (*)(void *))serve, csp);
             child_id = errno ? -1 : 0;
             pthread_attr_destroy(&attrs);
          }
