e74210ef8631ffd9469151bd70a66bc2c1d86ad9
fabiankeil <fabiankeil>
2007-09-15 13:01:31 +0000

    Increase listen() backlog to SOMAXCONN (or 128) to decrease chances of dropped connections under load. Problem reported and fix suggested by nobody in BR#1795281.

--- jbsockets.c
+++ jbsockets.c
@@ -1,4 +1,4 @@
-const char jbsockets_rcs[] = "$Id: jbsockets.c,v 1.43 2007/06/01 18:16:36 fabiankeil Exp $";
+const char jbsockets_rcs[] = "$Id: jbsockets.c,v 1.44 2007/09/15 13:01:31 fabiankeil Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/jbsockets.c,v $
@@ -35,6 +35,11 @@ const char jbsockets_rcs[] = "$Id: jbsockets.c,v 1.43 2007/06/01 18:16:36 fabian
  *
  * Revisions   :
  *    $Log: jbsockets.c,v $
+ *    Revision 1.44  2007/09/15 13:01:31  fabiankeil
+ *    Increase listen() backlog to SOMAXCONN (or 128) to decrease
+ *    chances of dropped connections under load. Problem reported
+ *    and fix suggested by nobody in BR#1795281.
+ *
  *    Revision 1.43  2007/06/01 18:16:36  fabiankeil
  *    Use the same mutex for gethostbyname() and gethostbyaddr() to prevent
  *    deadlocks and crashes on OpenBSD and possibly other OS with neither
@@ -305,6 +310,11 @@ const char jbsockets_h_rcs[] = JBSOCKETS_H_VERSION;
  * XXX: Does it make sense to make this a config option?
  */
 #define MAX_DNS_RETRIES 10
+
+#ifndef SOMAXCONN
+/* XXX: Might not be necessary. */
+#define SOMAXCONN 128
+#endif
 
 /*********************************************************************
  *
@@ -653,7 +663,7 @@ int bind_port(const char *hostnam, int portnum, jb_socket *pfd)
     * duplicate instances of Privoxy from being caught.
     *
     * On UNIX, we assume the user is sensible enough not
-    * to start Junkbuster multiple times on the same IP.
+    * to start Privoxy multiple times on the same IP.
     * Without this, stopping and restarting Privoxy
     * from a script fails.
     * Note: SO_REUSEADDR is meant to only take over
@@ -682,7 +692,7 @@ int bind_port(const char *hostnam, int portnum, jb_socket *pfd)
       }
    }
 
-   while (listen(fd, 5) == -1)
+   while (listen(fd, SOMAXCONN) == -1)
    {
       if (errno != EINTR)
       {
