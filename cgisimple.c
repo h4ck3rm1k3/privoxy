f357a30118a6f76d094fcc2b1c8cc18b9203993a
fabiankeil <fabiankeil>
2011-02-14 16:07:52 +0000

    In get_content_type(), also recognize '.jpeg' as JPEG extension

--- cgisimple.c
+++ cgisimple.c
@@ -1,4 +1,4 @@
-const char cgisimple_rcs[] = "$Id: cgisimple.c,v 1.105 2011/02/14 16:07:32 fabiankeil Exp $";
+const char cgisimple_rcs[] = "$Id: cgisimple.c,v 1.106 2011/02/14 16:07:52 fabiankeil Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/cgisimple.c,v $
@@ -684,6 +684,7 @@ static const char *get_content_type(const char *filename)
    {
       {".css",  "text/css"},
       {".jpg",  "image/jpeg"},
+      {".jpeg", "image/jpeg"},
       {".png",  "image/png"},
    };
 
