e2f502e2b0ac0b394af12f4f75884b7579c4c58f
fabiankeil <fabiankeil>
2011-07-17 13:37:14 +0000

    Minor style fix for unload_configfile()

--- loadcfg.c
+++ loadcfg.c
@@ -1,4 +1,4 @@
-const char loadcfg_rcs[] = "$Id: loadcfg.c,v 1.118 2011/07/17 13:34:36 fabiankeil Exp $";
+const char loadcfg_rcs[] = "$Id: loadcfg.c,v 1.119 2011/07/17 13:37:14 fabiankeil Exp $";
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/loadcfg.c,v $
@@ -231,7 +231,7 @@ static void unload_configfile (void * data)
 
    for (i = 0; i < MAX_LISTENING_SOCKETS; i++)
    {
-       freez(config->haddr[i]);
+      freez(config->haddr[i]);
    }
    freez(config->logfile);
 
