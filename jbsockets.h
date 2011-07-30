86aa342c26b0a36a7937b1370f36cccd930de0c2
fabiankeil <fabiankeil>
2011-07-30 15:17:35 +0000

    Deduplicate the INADDR_NONE definition for Solaris by moving it to jbsockets.h
    While at it, add a comment about whether or not the workaround is still needed.

--- jbsockets.h
+++ jbsockets.h
@@ -1,6 +1,6 @@
 #ifndef JBSOCKETS_H_INCLUDED
 #define JBSOCKETS_H_INCLUDED
-#define JBSOCKETS_H_VERSION "$Id: jbsockets.h,v 1.18 2011/07/17 13:34:36 fabiankeil Exp $"
+#define JBSOCKETS_H_VERSION "$Id: jbsockets.h,v 1.19 2011/07/30 15:17:35 fabiankeil Exp $"
 /*********************************************************************
  *
  * File        :  $Source: /cvsroot/ijbswa/current/jbsockets.h,v $
@@ -67,6 +67,15 @@ extern int socket_is_still_alive(jb_socket sfd);
 /* Revision control strings from this header and associated .c file */
 extern const char jbsockets_rcs[];
 extern const char jbsockets_h_rcs[];
+
+/*
+ * Solaris workaround
+ * XXX: still necessary?
+ */
+#ifndef INADDR_NONE
+#define INADDR_NONE -1
+#endif
+
 
 #ifdef __cplusplus
 } /* extern "C" */
